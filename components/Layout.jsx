15de5ea2cc5f94a976457078c888de0b2ea54a9b
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-03-06 23:03:32 +0530

    new

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,10 +5,31 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import MobileNav from './MobileNav';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
+import { useRouter } from 'next/router';
 
 const Layout = ({ children }) => {
 
   const [openMenu, setOpenMenu] = useState(false);
+  
+  const router = useRouter();
+
+  const handleSearch = (event) => {
+    if (event.key === 'Enter') {
+      router.push({
+        pathname: '/search',
+        query: { searchItem: searchItem }
+      });
+      setSearchItem("");
+    }
+  };
+
+  const handleSearchOnClick = () => {
+    router.push({
+      pathname: '/search',
+      query: { searchItem: searchItem }
+    });
+    setSearchItem("");
+  };
 
   return (
     <div className="layout">
@@ -20,8 +41,10 @@ const Layout = ({ children }) => {
         <Navbar openMenu={openMenu} setOpenMenu={setOpenMenu} />
       </header>
       <aside className="searchContainer2">
-        <input className='searchBar' type="text" placeholder='Search Products' />
-        <FaMagnifyingGlass />
+        <input className='searchBar' type="text" placeholder='Search Products' onKeyDown={handleSearch} />
+        <div onClick={handleSearchOnClick}>
+          <FaMagnifyingGlass style={{ color: 'white'}} />
+        </div>
       </aside>
       <main className="main-container">
         {children}
